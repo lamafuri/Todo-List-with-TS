4ec86b1b8a3ee9cdb4eec364e6e1c03590b8c9f8
Furi Lama <lamafuri60@gmail.com>
2026-01-02 08:21:23 +0545

    feat: add logic to create todos and update the UI

--- src/main.ts
+++ src/main.ts
@@ -18,3 +18,37 @@ let editTodoInput = document.getElementById('edit-todo-input') as HTMLInputEleme
 let todoList = document.getElementById('todo-list') as HTMLUListElement;
 let todoItemsNodes = todoList.children
 
+function addTodoToTodos() {
+  let myTodo = {
+
+    title: todoInput.value,
+    id: (todos.length == 0) ? 1 : Math.max(...todos.map(todo => todo.id + 1)),
+    isCompleted: false
+  }
+  todoInput.value = ""
+  todoInput.focus()
+  todos.push(myTodo);
+}
+function render() {
+  todoList.innerHTML = ""
+  todos.forEach((todo) => {
+    let UI = `<li class = "${todo.id} my">
+                  <input type="checkbox" id="${todo.id}">
+                  <label for="${todo.id}">
+                    ${todo.title}
+                  </label>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </li>`
+    todoList.innerHTML += UI
+  })
+}
+
+// Listeners
+form.addEventListener('submit', function (e: SubmitEvent) {
+  e.preventDefault()
+  addTodoToTodos()
+  render()
+  console.log(todoItemsNodes);
+
+})
